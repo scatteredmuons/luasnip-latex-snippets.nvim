3bcba887a6933e21581abb98d1ca7fefc56aa4cc
ada <nebulousiosis@proton.me>
2026-04-16 00:06:14 -0500

    added definition and box

--- lua/luasnip-latex-snippets/bwA.lua
+++ lua/luasnip-latex-snippets/bwA.lua
@@ -31,89 +31,98 @@ function M.retrieve(not_math)
 
 s({ trig = "cct", name = "Unnumbered subsubsection" },
 { t({ "\\subsubsectionU{" }), i(1), t({ "}" }) }
-    ),
+ ),
 
-    s({ trig = "Sct", name = "Numbered section" },
-    { t({ "\\section{" }), i(1), t({ "}" }) }
-  ),
+ s({ trig = "Sct", name = "Numbered section" },
+ { t({ "\\section{" }), i(1), t({ "}" }) }
+ ),
 
-  s({ trig = "Bct", name = "Numbered subsection" },
-  { t({ "\\subsection{" }), i(1), t({ "}" }) }
+ s({ trig = "Bct", name = "Numbered subsection" },
+ { t({ "\\subsection{" }), i(1), t({ "}" }) }
 ),
 
 s({ trig = "Cct", name = "Numbered subsubsection" },
 { t({ "\\subsubsection{" }), i(1), t({ "}" }) }
-    ),
+ ),
 
-    s({ trig = "beq", name = "Numbered equation" },
-    { t({ "\\begin{equation}", "\t" }), i(1), t({ "", "\\end{equation}" }) }
-  ),
+ s({ trig = "beq", name = "Numbered equation" },
+ { t({ "\\begin{equation}", "\t" }), i(1), t({ "", "\\end{equation}" }) }
+ ),
 
-  s({ trig = "rseq", name = "Reset equation counter" },
-  { t({ "\\setcounter{equation}{0}" }) }
+ s({ trig = "rseq", name = "Reset equation counter" },
+ { t({ "\\setcounter{equation}{0}" }) }
 ),
 
 s({ trig = "enum", name = "Enumerate" },
 { t({ "\\begin{enumerate}", "\t" }), i(1), t({ "", "\\end{enumerate}" }) }
-    ),
+ ),
 
-    s({ trig = "prop", name = "Proposition (no number)" },
-    { t({ "\\begin{proposition*}", "\t" }), i(1), t({ "", "\\end{proposition*}" }) }
-  ),
+ s({ trig = "prop", name = "Proposition (no number)" },
+ { t({ "\\begin{proposition*}", "\t" }), i(1), t({ "", "\\end{proposition*}" }) }
+ ),
 
-  s({ trig = "pron", name = "Numbered proposition" },
-  { t({ "\\begin{proposition}", "\t" }), i(1), t({ "", "\\end{proposition}" }) }
+ s({ trig = "pron", name = "Numbered proposition" },
+ { t({ "\\begin{proposition}", "\t" }), i(1), t({ "", "\\end{proposition}" }) }
+),
+
+-- custom tcolorbox 
+s({ trig = "box", name = "Box" },
+{ t({ "\\begin{box}", "\t" }), i(1), t({ "", "\\end{box}" }) }
+),
+
+s({ trig = "def", name = "Definition" },
+{ t({ "\\begin{definition}", "\t" }), i(1), t({ "", "\\end{definition}" }) }
 ),
 
 s({ trig = "examp", name = "Example" },
 { t({ "\\begin{example}", "\t" }), i(1), t({ "", "\\end{example}" }) }
-    ),
+ ),
 
-    s({ trig = "sltn", name = "Solution" },
-    { t({ "\\begin{solution}", "\t" }), i(1), t({ "", "\\end{solution}" }) }
-  ),
+ s({ trig = "sltn", name = "Solution" },
+ { t({ "\\begin{solution}", "\t" }), i(1), t({ "", "\\end{solution}" }) }
+ ),
 
-  s({ trig = "proof", name = "Proof" },
-  { t({ "\\begin{proof}", "\t" }), i(1), t({ "", "\\end{proof}" }) }
+ s({ trig = "proof", name = "Proof" },
+ { t({ "\\begin{proof}", "\t" }), i(1), t({ "", "\\end{proof}" }) }
 ),
 
 s({ trig = "itemize", name = "Itemize" },
 { t({ "\\begin{itemize}", "\t" }), i(1), t({ "", "\\end{itemize}" }) }
-    ),
+ ),
 
-    s({ trig = "iit", name = "item" },
-    { t({ "\\item" }), i(1) }
-  ),
+ s({ trig = "iit", name = "item" },
+ { t({ "\\item" }), i(1) }
+ ),
 
-  s({ trig = "ali", name = "Align" },
-  { t({ "\\begin{align*}", "\t" }), i(1), t({ "", "\\end{align*}" }) }
+ s({ trig = "ali", name = "Align" },
+ { t({ "\\begin{align*}", "\t" }), i(1), t({ "", "\\end{align*}" }) }
 ),
 
 s({ trig = "nli", name = "Numbered align" },
 { t({ "\\begin{align}", "\t" }), i(1), t({ "", "\\end{align}" }) }
-    ),
+ ),
 
-    parse_snippet({ trig = "beg", name = "begin{} / end{}" }, "\\begin{$1}\n\t$0\n\\end{$1}"),
+ parse_snippet({ trig = "beg", name = "begin{} / end{}" }, "\\begin{$1}\n\t$0\n\\end{$1}"),
 
-    s({ trig = "bx", name = "Boxed equation" },
-    { t({ "\\[ \\boxed{", "\t" }), i(1), t({ "", "}\\]" }) }
-  ),
+ s({ trig = "bx", name = "Boxed equation" },
+ { t({ "\\[ \\boxed{", "\t" }), i(1), t({ "", "}\\]" }) }
+ ),
 
-  s({ trig = "bn", name = "Boxed numbered equation" },
-  { t({ "\\begin{equation} \\boxed{", "\t" }), i(1), t({ "", "} \\end{equation}" }) }
+ s({ trig = "bn", name = "Boxed numbered equation" },
+ { t({ "\\begin{equation} \\boxed{", "\t" }), i(1), t({ "", "} \\end{equation}" }) }
 ),
 
 --these require the tcolorbox package
 s({ trig = "rbox", name = "Red box" },
 { t({ "\\begin{tcolorbox}[colback=red!5!white,colframe=red!75!black]", "\t" }), i(1), t({ "", "\\end{tcolorbox}" }) }
-    ),
+ ),
 
-    s({ trig = "gbox", name = "Green box" },
-    { t({ "\\begin{tcolorbox}[colback=green!5!white,colframe=green!75!black]", "\t" }), i(1), t({ "", "\\end{tcolorbox}" }) }
-  ),
+ s({ trig = "gbox", name = "Green box" },
+ { t({ "\\begin{tcolorbox}[colback=green!5!white,colframe=green!75!black]", "\t" }), i(1), t({ "", "\\end{tcolorbox}" }) }
+ ),
 
-  s({ trig = "bbox", name = "Blue box" },
-  { t({ "\\begin{tcolorbox}[colback=blue!5!white,colframe=blue!75!black]", "\t" }), i(1), t({ "", "\\end{tcolorbox}" }) }
+ s({ trig = "bbox", name = "Blue box" },
+ { t({ "\\begin{tcolorbox}[colback=blue!5!white,colframe=blue!75!black]", "\t" }), i(1), t({ "", "\\end{tcolorbox}" }) }
 ),
 
 s({ trig = "bigfun", name = "Big function" }, {
